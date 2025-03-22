88c2f921fea272b57822e7de4105778a1a4120ff
Limber Tolaba <limber.rodrigo.tj@gmail.com>
2025-03-21 23:38:39 -0400

    Omision de la comprobacion Eslint

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,11 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    ppr: 'incremental'
-  }
+    ppr: 'incremental',
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Esta línea desactiva ESLint en la compilación
+  },
 };
 
 export default nextConfig;
